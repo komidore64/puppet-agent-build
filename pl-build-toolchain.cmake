4f38129162bbb80e2d51393eb7d268c22ed2f8e0
Eric D. Helms <ericdhelms@gmail.com>
2016-09-02 09:39:55 -0400

    Updates to build GCC and work on EL6 x86_64

--- pl-build-toolchain.cmake
+++ pl-build-toolchain.cmake
@@ -2,5 +2,9 @@ SET(BOOST_ROOT /opt/pl-build-tools)
 SET(BOOST_INCLUDE_DIRS /opt/pl-build-tools/include)
 SET(BOOST_LIBRARY_DIRS /opt/pl-build-tools/lib)
 
-SET(CMAKE_C_COMPILER /bin/gcc)
-SET(CMAKE_CXX_COMPILER /bin/g++)
+SET(CMAKE_C_COMPILER /opt/pl-build-tools/bin/gcc)
+SET(CMAKE_CXX_COMPILER /opt/pl-build-tools/bin/g++)
+
+SET(CMAKE_INCLUDE_PATH /opt/pl-build-tools/include)
+SET(CMAKE_LIBRARY_PATH /opt/pl-build-tools/lib)
+SET(CMAKE_PREFIX_PATH /opt/pl-build-tools)
